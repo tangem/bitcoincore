d2936ed7216560dbf0f8bbb267beb5dacf995d15
Andrey Fedorov <afedorov@tangem.com>
2024-10-22 00:24:46 +0200

    IOS-8290: Fix SPM target file path
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "BitcoinCore",
-            path: "BitcoinCore"
+            path: "BitcoinCore/Classes"
         ),
         .testTarget(
             name: "BitcoinCoreTests",
